6b0a25c0cbe9020ed8e955af081940b25bd462e6
jinux <jinguoliang@foundao.com>
2019-06-02 11:36:32 +0800

    export host string

--- lib/gtd/api/ming_server.dart
+++ lib/gtd/api/ming_server.dart
@@ -3,8 +3,10 @@ import 'dart:io';
 
 import 'package:flutter_app/gtd/model/TodoItem.dart';
 
+final String _HOST = "118.89.57.250";
+
 void requestTodayTodos(Function fun) async {
-  final uri = Uri.http("118.89.57.250", '/api/todos', {});
+  final uri = Uri.http(_HOST, '/api/todos', {});
   final httpClient = HttpClient();
   final httpRequest = await httpClient.getUrl(uri);
   final httpResponse = await httpRequest.close();
