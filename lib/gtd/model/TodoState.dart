625c45a61462d9bf8037349d791d061069d4041f
jinux <jinguoliang@foundao.com>
2019-06-02 13:33:39 +0800

    add TODO state and make it as default

--- lib/gtd/model/TodoState.dart
+++ lib/gtd/model/TodoState.dart
@@ -1,14 +1,16 @@
-enum TodoState { DONE, DELAY, CLOCKING }
+enum TodoState { DONE, DELAY, CLOCKING, TODO }
 
 TodoState getStateFromString(state) {
   switch (state) {
     case "DONE":
       return TodoState.DONE;
+    case "TODO":
+      return TodoState.TODO;
     case "DELAY":
       return TodoState.DELAY;
     case "CLOCKING":
       return TodoState.CLOCKING;
     default:
-      return TodoState.DONE;
+      return TodoState.TODO;
   }
 }
